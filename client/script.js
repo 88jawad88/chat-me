3c308c1a171e4409883a96cbc3aca298a43e5391
Jawad Halawa <60158514+88jawad88@users.noreply.github.com>
2023-01-05 12:58:28 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
   // https://codex-im0y.onrender.com/
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://chat-me-iku1.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
